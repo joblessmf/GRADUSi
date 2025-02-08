c7bb47b8930214b72aeccd7be049e4697c7e073d
nikolaysimeonov <ndimitrov992@gmail.com>
2025-02-08 14:28:55 +0200

    Initial Commit

---
+++ GRADUSiTests/GRADUSiTests.swift
@@ -0,0 +1,17 @@
+//
+//  GRADUSiTests.swift
+//  GRADUSiTests
+//
+//  Created by Nikolay Simeonov on 8.02.25.
+//
+
+import Testing
+@testable import GRADUSi
+
+struct GRADUSiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
